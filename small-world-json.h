a136c76225ae477b418c31787479cc8d1d4c0e35
ZXC <zhxxch@outlook.com>
2020-07-01 10:43:19 +0800

    开始重构小世界数据结构

--- small-world-json.h
+++ small-world-json.h
@@ -13,20 +13,22 @@ int js_edge_footer(FILE *out_stream) {
 	return fprintf(out_stream, "];\n");
 }
 int js_node(FILE *out_stream, const int nodeID) {
-	return fprintf(out_stream, "{id:%i},\n", nodeID);
+	return fprintf(out_stream, "{id:\"%i\"},\n", nodeID);
 }
 int js_edge(FILE *out_stream, const int srcID,
 	const int dstID) {
 	return fprintf(out_stream,
-		"{source:%i,target:%i},\n", srcID, dstID);
+		"{source:\"%i\",target:\"%i\"},\n", srcID,
+		dstID);
 }
 int HasNode(const int EdgesA[], const int EdgesB[],
 	const int NumEdges, const int nodeID);
-int print_graph_js(FILE *out_stream, const int EdgesA[], const int EdgesB[], const int NumNodes, const int NumEdges){
+int print_graph_js(FILE *out_stream,
+	const int EdgesA[], const int EdgesB[],
+	const int NumNodes, const int NumEdges) {
 	js_node_header(out_stream);
 	for(int i = 0; i < NumNodes; i++) {
-		if(HasNode(
-			   EdgesA, EdgesB, NumEdges, i)) {
+		if(HasNode(EdgesA, EdgesB, NumEdges, i)) {
 			js_node(out_stream, i);
 		}
 	}
